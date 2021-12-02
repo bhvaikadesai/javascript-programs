f370e49c01458cdc02d4196c89556c94adad7362
bhavika <bhavika08.exhibyte@gmail.com>
2021-12-02 13:02:38 +0530

    new code add

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("hello world!");
+    
+let js = "good";
+if(js === 'good') alert('confirm query');   
+
+console.log(40 + 8 +23 -10);
+console.log("jonas");
+console.log(23);
+var firstname = "jonas";
+console.log(firstname);
