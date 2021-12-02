f46f8ea1c1f700abc722efb662e76ea18b4b6ae4
bhvaikadesai <bhavika08.exhibyte@gmail.com>
2021-12-02 10:03:30 +0530

    new first program

--- 1.js
+++
@@ -1,9 +0,0 @@
-const populations = [10, 1441, 332, 83];
-console.log(populations.length === 4);
-const percentages = [
-percentageOfWorld1(populations[0]),
-percentageOfWorld1(populations[1]),
-percentageOfWorld1(populations[2]),
-percentageOfWorld1(populations[3])
-];
-console.log(percentages);
