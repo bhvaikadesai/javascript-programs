2c87307f7daf7be50477facab090f18069d4c1e6
bhavika <bhavika08.exhibyte@gmail.com>
2021-12-03 13:50:33 +0530

    add javascript code

--- script.js
+++ script.js
@@ -48,27 +48,58 @@
 // const birthyear2 = 1998;
 // console.log(birthyear - '' - birthyear2);
 
-const massmask = 78;
-const heightmask = 1.69;
-const massjohn = 92;
-const heightjohn = 1.95;
+// const massmask = 78;
+// const heightmask = 1.69;
+// const massjohn = 92;
+// const heightjohn = 1.95;
 
 // const massmask = 95;
 // const heightmask = 1.88;
 // const massjohn = 85;
 // const heightjohn = 1.76;
 
-const BMImask = massmask / heightmask ** 2;
-const BMIjohn = massjohn / (heightjohn * heightjohn);
+// const BMImask = massmask / heightmask ** 2;
+// const BMIjohn = massjohn / (heightjohn * heightjohn);
 
-console.log(BMImask, BMIjohn);
+// console.log(BMImask, BMIjohn);
 
-if(BMImask > BMIjohn){
-    console.log(`mark's BMI (${BMImask}) is higher then john's (${BMIjohn})!`)
-}else{
-    console.log(`john's BMI (${BMIjohn}) is higher then mask's! (${BMImask})`)
+// if(BMImask > BMIjohn){
+//     console.log(`mark's BMI (${BMImask}) is higher then john's (${BMIjohn})!`)
+// }else{
+//     console.log(`john's BMI (${BMIjohn}) is higher then mask's! (${BMImask})`)
+// }
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+
+// const money = 100;
+// if(money){
+//     console.log("Don't speed it all;)");
+// }else{
+//     console.log("You should get a job!");
+// }
+
+const age = '20';
+if (age === 20) console.log('you just became an adult :D (strict)');
+if(age == 20) console.log('you just beacame an adult :D (loose)')
+
+const favourit = Number(prompt("What's Is Your Age?"));
+console.log(favourit);
+console.log(typeof favourit);
+
+
+if(favourit == 25){
+    console.log("what favourit is true?");
+}else(
+    console.log("what favourit is false?")
+)
+
+if(favourit !== 25){
+    console.log("why favourit not 20?")
 }
-
-
 
 
